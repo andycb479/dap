319dffcb66f8cc8a9a2d6efe09e15b37685224c8
Ciobanu Andy <andyciobanu17@gmail.com>
2022-09-19 22:31:17 +0300

    Initial Chat Sesssion service setup

--- ChatApp/ChatSessionService/Program.cs
+++ ChatApp/ChatSessionService/Program.cs
@@ -1,4 +1,5 @@
 using ChatSessionService.Services;
+using Services.Core.ServiceDiscovery;
 
 var builder = WebApplication.CreateBuilder(args);
 
@@ -7,11 +8,22 @@ var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
 builder.Services.AddGrpc();
+// builder.Services.AddConsul(builder.Configuration.GetServiceConfig());
 
 var app = builder.Build();
 
 // Configure the HTTP request pipeline.
-app.MapGrpcService<GreeterService>();
-app.MapGet("/", () => "Communication with gRPC endpoints must be made through a gRPC client. To learn how to create a client, visit: https://go.microsoft.com/fwlink/?linkid=2086909");
+app.UseRouting();
+
+app.UseEndpoints(endpoints =>
+{
+     endpoints.MapGrpcService<GreeterService>();
+     endpoints.MapGrpcService<HealthCheckService>();
+
+     endpoints.MapGet("/", async context =>
+     {
+          await context.Response.WriteAsync("Communication with gRPC endpoints must be made through a gRPC client. To learn how to create a client, visit: https://go.microsoft.com/fwlink/?linkid=2086909");
+     });
+});
 
 app.Run();
