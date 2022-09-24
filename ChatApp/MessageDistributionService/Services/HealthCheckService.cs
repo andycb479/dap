c4e14faffe855dacc0483d4c4a4a666e6c1e6ebe
Ciobanu Andy <andyciobanu17@gmail.com>
2022-09-24 18:28:58 +0300

    Added messsage distribution service.

---
+++ ChatApp/MessageDistributionService/Services/HealthCheckService.cs
@@ -0,0 +1,19 @@
+﻿using Grpc.Core;
+using Grpc.Health.V1;
+
+namespace MessageDistributionService.Services
+{
+     public class HealthCheckService : Health.HealthBase
+     {
+          public override Task<HealthCheckResponse> Check(HealthCheckRequest request, ServerCallContext context)
+          {
+               return Task.FromResult(new HealthCheckResponse() { Status = HealthCheckResponse.Types.ServingStatus.Serving });
+          }
+
+          public override async Task Watch(HealthCheckRequest request, IServerStreamWriter<HealthCheckResponse> responseStream, ServerCallContext context)
+          {
+               await responseStream.WriteAsync(new HealthCheckResponse()
+               { Status = HealthCheckResponse.Types.ServingStatus.Serving });
+          }
+     }
+}
