7349a4ca3d0967acba74dea1dfae39ada0fe525d
radu-arama4 <rarama33@gmail.com>
2022-09-29 00:24:31 +0300

    feat: user service setup

---
+++ ChatApp/Users/src/main/java/com/pad/Users/entity/User.java
@@ -0,0 +1,15 @@
+package com.pad.Users.entity;
+
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.Id;
+
+@Entity
+public class User {
+    @Id
+    @GeneratedValue
+    private Long id;
+    private String firstName;
+    private String lastName;
+    private String status;
+}
