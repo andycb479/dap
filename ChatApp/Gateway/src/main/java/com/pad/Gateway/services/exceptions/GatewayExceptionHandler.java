7a7f6a62f568970077b88573d5d8842fb7ad54da
radu-arama4 <rarama33@gmail.com>
2022-11-01 22:44:29 +0200

    feat: circuit breaker configuration and exception handling

--- ChatApp/Gateway/src/main/java/com/pad/Gateway/services/exceptions/GatewayExceptionHandler.java
+++ ChatApp/Gateway/src/main/java/com/pad/Gateway/services/exceptions/GatewayExceptionHandler.java
@@ -1,5 +1,6 @@
 package com.pad.Gateway.services.exceptions;
 
+import io.github.resilience4j.circuitbreaker.CallNotPermittedException;
 import io.grpc.StatusRuntimeException;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.http.HttpStatus;
@@ -9,9 +10,9 @@ import org.springframework.web.bind.annotation.ExceptionHandler;
 
 @ControllerAdvice
 @Slf4j
-public class GrpcExceptionHandler {
+public class GatewayExceptionHandler {
   @ExceptionHandler(value = StatusRuntimeException.class)
-  public ResponseEntity<Object> exception(StatusRuntimeException exception) {
+  public ResponseEntity<Object> statusRuntimeException(StatusRuntimeException exception) {
     log.error(exception.getStatus().toString());
 
     switch (exception.getStatus().getCode()) {
@@ -32,5 +33,11 @@ public class GrpcExceptionHandler {
             exception.getStatus().getCode() + ": " + exception.getStatus().getDescription(),
             HttpStatus.INTERNAL_SERVER_ERROR);
     }
+  }
+
+  @ExceptionHandler(value = CallNotPermittedException.class)
+  public ResponseEntity<Object> resilienceException(CallNotPermittedException exception) {
+    log.error(exception.getMessage());
+    return new ResponseEntity<>(exception.getMessage(), HttpStatus.SERVICE_UNAVAILABLE);
   }
 }
--- ChatApp/Gateway/src/main/java/com/pad/Gateway/services/util/CircuitBreakerContext.java
+++ ChatApp/Gateway/src/main/java/com/pad/Gateway/services/util/CircuitBreakerContext.java
@@ -3,6 +3,8 @@ package com.pad.Gateway.services.util;
 import io.github.resilience4j.circuitbreaker.CircuitBreaker;
 import io.github.resilience4j.circuitbreaker.CircuitBreakerConfig;
 import io.github.resilience4j.circuitbreaker.CircuitBreakerRegistry;
+import io.grpc.Status;
+import io.grpc.StatusRuntimeException;
 import lombok.extern.slf4j.Slf4j;
 
 @Slf4j
@@ -17,6 +19,14 @@ public class CircuitBreakerContext {
               .slidingWindowType(CircuitBreakerConfig.SlidingWindowType.COUNT_BASED)
               .slidingWindowSize(10)
               .failureRateThreshold(2.0f)
+              .ignoreException(
+                  throwable -> {
+                    if (throwable instanceof StatusRuntimeException) {
+                      Status.Code code = ((StatusRuntimeException) throwable).getStatus().getCode();
+                      return code.equals(Status.Code.INVALID_ARGUMENT);
+                    }
+                    return false;
+                  })
               .build();
       CircuitBreakerRegistry registry = CircuitBreakerRegistry.of(config);
       CircuitBreaker circuitBreaker = registry.circuitBreaker("userService");
@@ -33,6 +43,14 @@ public class CircuitBreakerContext {
               .slidingWindowType(CircuitBreakerConfig.SlidingWindowType.COUNT_BASED)
               .slidingWindowSize(10)
               .failureRateThreshold(2.0f)
+              .ignoreException(
+                  throwable -> {
+                    if (throwable instanceof StatusRuntimeException) {
+                      Status.Code code = ((StatusRuntimeException) throwable).getStatus().getCode();
+                      return code.equals(Status.Code.INVALID_ARGUMENT);
+                    }
+                    return false;
+                  })
               .build();
       CircuitBreakerRegistry registry = CircuitBreakerRegistry.of(config);
       CircuitBreaker circuitBreaker = registry.circuitBreaker("chatService");
